853e6eb4a8f3d983a3d31796d380a9ad78dfd20e
YanethM <yaneth.mejiar@autonoma.edu.co>
2023-03-09 17:34:43 -0500

    Initial project structure

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { UserForm } from "./src/components/UserForm";
+import { Pressable, SafeAreaView, StyleSheet, Text, Modal } from "react-native";
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalUserForm, setModalUserForm] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Registrate en la {""}
+        <Text style={styles.titleBold}>UAM</Text>
+      </Text>
+
+      <Pressable onPress={()=>setModalVisible(true)} style={styles.btnNewUser}>
+        <Text style={styles.titleButton}>Nuevo usuario</Text>
+      </Pressable>
+
+      <Pressable onPress={()=>setModalUserForm(true)} style={styles.btnNewUser}>
+        <Text style={styles.titleButton}>Nuevo usuario</Text>
+      </Pressable>
+      <UserForm modalUserForm = {modalUserForm}></UserForm>
+
+      <Modal animationType="slide" visible={modalVisible}>
+        <Text>Desde modal</Text>
+      </Modal>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#0069a3",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 22,
+    color: "#FFFFFF",
+  },
+  titleBold: {
+    fontWeight: "900",
+    color: "#f4d73b",
+  },
+  btnNewUser: {
+    backgroundColor: "#f4d73b",
+    padding: 10,
+    marginTop: 30,
+    marginHorizontal: 20,
+    borderRadius: 10,
+  },
+  titleButton: {
+    textAlign: "center",
+    fontSize: 20,
+    color: "#000000",
   },
 });
---
+++ src/components/UserForm.js
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import {
+  Image,
+  Modal,
+  StyleSheet,
+  ImageBackground,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  Pressable,
+} from "react-native";
+import DatePicker from "react-native-date-picker";
+
+export const UserForm = ({ modalUserForm }) => {
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
+  return (
+    <Modal animationType="slide" visible={modalUserForm}>
+      <ImageBackground
+        style={styles.backCover}
+        source={require("../assets/jpg/dev2.jpg")}
+      >
+        <Image
+          style={styles.image}
+          source={require("../assets/png/Logos_UAM-08.png")}
+        />
+        <ScrollView>
+          <Text style={styles.title}>
+            Inscripción {""}
+            <Text style={styles.titleBold}>Vacaciones UAM</Text>
+          </Text>
+          <View style={styles.campo}>
+            <Text style={styles.text}>Nuevo usuario</Text>
+            <TextInput
+              placeholder="Nombre completo"
+              placeholderTextColor={"#F8F9F9"}
+              style={styles.input}
+            ></TextInput>
+          </View>
+
+          <View style={styles.campo}>
+            <Text style={styles.text}>Correo</Text>
+            <TextInput
+              placeholder="@autonoma.edu.co"
+              placeholderTextColor={"#F8F9F9"}
+              style={styles.input}
+              keyboardType="email-address"
+            ></TextInput>
+          </View>
+          <View style={styles.campo}>
+            <Text style={styles.text}>Celular</Text>
+            <TextInput
+              placeholder="Celular"
+              placeholderTextColor={"#F8F9F9"}
+              style={styles.input}
+              keyboardType="phone-pad"
+            ></TextInput>
+          </View>
+          <View style={styles.campo}>
+            <Pressable onPress={() => setOpen(true)}>
+              <Text style={styles.text}>Fecha inscripción</Text>
+            </Pressable>
+            <DatePicker
+              modal
+              open={open}
+              date={date}
+              mode={"date"}
+              onConfirm={(date) => {
+                setOpen(false);
+                setDate(date);
+              }}
+              onCancel={() => {
+                setOpen(false);
+              }}
+            />
+          </View>
+        </ScrollView>
+      </ImageBackground>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    margin: 15,
+    width: 75,
+    height: 75,
+    marginBottom: 15,
+  },
+
+  backCover: {
+    position: "absolute",
+    marginTop: 0,
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    opacity: 0.7,
+    backgroundColor: "rgba(52, 52, 52, alpha)",
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 22,
+    color: "#FFFFFF",
+    marginHorizontal: 30,
+    fontWeight: "600",
+    marginBottom: 20,
+  },
+  titleBold: {
+    textAlign: "center",
+    fontSize: 22,
+    color: "#0069a3",
+    marginHorizontal: 30,
+    fontWeight: "600",
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: "#000000c0",
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 20,
+    color: "#FFF",
+    marginBottom: 8,
+    marginTop: 12,
+  },
+  campo: {
+    marginHorizontal: 30,
+  },
+});
